841e5106c603e2c63407f41a56f7bfbf7704f440
a-nunez0 <122509183+a-nunez0@users.noreply.github.com>
2026-04-25 19:46:08 -0600

    Upload Node.js file intro

---
+++ controllers/home.js
@@ -0,0 +1,7 @@
+const getHome = (req, res) => {
+  res.send("Hi, my name is Alvaro Nuñez. I am studying Software Engineering and I enjoy building websites and playing soccer. Im not an edoert using node js and i love creaing animations using css.");
+};
+
+module.exports = {
+  getHome,
+};
