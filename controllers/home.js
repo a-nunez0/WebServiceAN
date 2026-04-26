e5cf5682b5061987b734ea877c1ea9adf34368f5
a-nunez0 <122509183+a-nunez0@users.noreply.github.com>
2026-04-25 20:27:40 -0600

    new

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,5 @@
 const getHome = (req, res) => {
-  res.send("Hi, my name is Alvaro Nuñez. I am studying Software Engineering and I enjoy building websites and playing soccer. Im not an edoert using node js and i love creaing animations using css.");
+  res.send("Hi, my name is Alvaro Nunez. I am studying Software Engineering and I enjoy building websites and playing soccer. Im not an expert using node js and i love creating animations using css.");
 };
 
 module.exports = {
