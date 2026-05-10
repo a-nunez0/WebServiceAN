9b30b0a9d0ef3557d8417ce11a7e876d8d4f6174
a-nunez0 <122509183+a-nunez0@users.noreply.github.com>
2026-05-09 21:03:37 -0600

    new changes

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -16,7 +16,52 @@ const getContactById = async (req, res) => {
   res.json(contact);
 };
 
+const createContact = async (req, res) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+
+  const response = await getDatabase().collection("contacts").insertOne(contact);
+
+  res.status(201).json(response.insertedId);
+};
+
+const updateContact = async (req, res) => {
+  const contactId = req.params.id;
+
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+
+  await getDatabase()
+    .collection("contacts")
+    .replaceOne({ _id: new ObjectId(contactId) }, contact);
+
+  res.sendStatus(204);
+};
+
+const deleteContact = async (req, res) => {
+  const contactId = req.params.id;
+
+  await getDatabase()
+    .collection("contacts")
+    .deleteOne({ _id: new ObjectId(contactId) });
+
+  res.sendStatus(200);
+};
+
 module.exports = {
   getAllContacts,
   getContactById,
+  createContact,
+  updateContact,
+  deleteContact,
 };
